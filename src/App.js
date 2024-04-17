0e90a1978931222d05c1446ecb49e87d1e87fd3b
AstroShibaInu <92301695+AstroShibaInu@users.noreply.github.com>
2024-04-17 16:40:00 +0200

    actualizacion
    actualizacion

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {light} from './styles/Themes';
 
 import Navigation from './components/Navigation';
 import Home from './components/sections/Home';
+import Video from './components/sections/Video';
 import About from './components/sections/About';
 import Roadmap from './components/sections/Roadmap';
 import Showcase from './components/sections/Showcase';
@@ -22,6 +23,7 @@ function App() {
       <ThemeProvider theme={light}>
         <Navigation />
         <Home />
+        <Video />
         <About />
         <Roadmap />
         <Tokeninfo />
